83f3ad9602292f2ca86e64a91ba99fa35ec427d1
hefan <hefan0412>
2023-10-25 15:44:32 +0800

    读取参数修改

--- Config/Program.cs
+++ Config/Program.cs
@@ -9,35 +9,22 @@ namespace ConfigExe
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
-            //ExcelConfig.Start(args[0], args[1]);
-            //foreach (var item in args)
-            //{
-            //    Console.WriteLine($"Path{item.Split(';')[0]}");
-            //}
-            //Console.WriteLine(args.Length);
-            ////Console.WriteLine(args.Length);
-            var bb = "D:/HanZhi/策划/Config|D:/HanZhi/Client/GamePlay/Assets/Scripts/Config/|D:/HanZhi/Client/GamePlay/Config/|D:\\HanZhi\\Client\\GamePlay\\Library\\ScriptAssemblies\\Assembly-CSharp.dll|D:\\HanZhi\\Client\\GamePlay\\Library\\ScriptAssemblies\\Assembly-CSharp.dll";
-            //var a = args[0].Split("|");
-            var a = bb.Split("|");
+            var a = args[0].Split("|");
+            //var p = "D:/HanZhi/Client/GamePlay/Library/ScriptAssemblies/Assembly-CSharp.dll";
+            //var a = "D:/HanZhi/策划/Config|D:/HanZhi/Client/GamePlay/Assets/Scripts/Config/|D:/HanZhi/Client/GamePlay/Config/|D:/HanZhi/Client/GamePlay/Config//AllConfig.cfg".Split("|");
 
-            //foreach (var b in a)
-            //{
-            //    Console.WriteLine(b);
-            //}
+            ///第一个是 策划配置路径，二是代码生成路径，3是每个配置生成的byte 路径，4是总配置合并路径 ，4后面都是   typeof(GameLaunch).Assembly.Location dll 路基
             InstanceT.Assemblies.Clear();
-            Console.WriteLine(a[0]);
-            Console.WriteLine(a[1]);
-            Console.WriteLine(a[2]);
-            Console.WriteLine(Assembly.LoadFile(a[3]));
-            for (int i = 3; i < a.Length; i++)
+
+            //第3个开始都是Assembly
+            for (int i = 4; i < a.Length; i++)
             {
                 InstanceT.AddAssembly(Assembly.LoadFile(a[i]));
             }
             InstanceT.AddAssembly(typeof(IFSerializable).Assembly);
-            Console.WriteLine(InstanceT.Assemblies.Count);
-            F.ExcelConfig.Start(a[0], a[1], a[2]);
-
+            //InstanceT.AddAssembly(Assembly.LoadFile(p));
+            //Console.WriteLine(InstanceT.Assemblies.Count);
+            F.ExcelConfig.Start(a[0], a[1], a[2], a.Length >= 3 ? a[3] : null);
         }
     }
 }
